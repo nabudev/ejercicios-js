1f2af0d03cf50fca7872449e5abeae9a23f5fe91
nahueldev <nabudev01@gmail.com>
2024-04-16 21:52:57 -0300

    correccion de detalle al mostrar los multiplos de 4 y 9: muestro el mensaje en la misma linea del numero para evitar que se repita

--- ejercicio-25/script.js
+++ ejercicio-25/script.js
@@ -2,12 +2,17 @@ function esMultiplo(numero, divisor) {
     return numero % divisor === 0;
 }
 for (let i = 1; i <= 500; i++) {
-    console.log(i);
+    let mensaje = '';
     if (esMultiplo(i, 4)) {
-        console.log(`${i} (Múltiplo de 4)`);
+        mensaje += `(Múltiplo de 4)`;
     }
     if (esMultiplo(i, 9)) {
-        console.log(`${i} (Múltiplo de 9)`);
+        mensaje += `(Múltiplo de 9)`;
+    }
+    if (mensaje !== '') {
+        console.log(`${i} ${mensaje}`);
+    } else {
+        console.log(i);
     }
     if (i % 5 === 0 && i < 500) {
         console.log("————————————————————");
